11d64dd143b931a7bd65be9c21fe08212414f2d2
Felipe Fernandes <mewbi.fernandes@gmail.com>
2023-07-23 22:05:20 -0300

    More sql solutions

---
+++ postgresql/sql/3001.sql
@@ -0,0 +1,36 @@
+--- URI Online Judge SQL
+--- Copyright URI Online Judge
+--- www.urionlinejudge.com.br
+--- Problem 3001
+
+CREATE TABLE products (
+id numeric PRIMARY KEY,
+name varchar(50),
+type char,
+price numeric
+);
+
+GRANT SELECT ON products TO sql_user;
+
+INSERT INTO products (id , name, type, price)
+VALUES
+(1, 'Monitor',  'B', 0),
+(2, 'Headset',  'A', 0),
+(3, 'PC Case',  'A', 0),
+(4, 'Computer Desk', 'C', 0),
+(5, 'Gaming Chair', 'C', 0),
+(6, 'Mouse',  'A', 0);
+
+/*  Execute this query to drop the tables */
+-- DROP TABLE products;
+
+SELECT 
+    name,
+    CASE
+        WHEN (type = 'A') THEN 20.0
+        WHEN (type = 'B') THEN 70.0
+        WHEN (type = 'C') THEN 530.5
+    END AS price
+FROM
+    products
+ORDER BY type, id DESC;
